1eddc01d66a8b717434c818811ae8263ceba4858
batloltoman <93631837+batloltoman@users.noreply.github.com>
2021-12-10 12:58:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,34 +19,35 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+This is Permanent Files Store Bot of [*☂️Renegades☂️**](https://t.me/cp_renegade)
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
 
-📝 **Language:** [Python3](https://www.python.org)
+🤖 **My Name:** [You Know Already 😉]
 
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+
+
+
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+👥 **Support Group:** [Renegades](https://t.me/renegadesforever)
+
+📢 **Channel:** [Renegade](https://t.me/cp_renegade)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @cp_renegade
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
-"""
+
+
+
+
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot** of [Renegade](https://t.me/cp_renegade)
 
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
-"""
+
+Owned By [Team Renegades](https://t.me/cp_renegade)
+Check about bot button
